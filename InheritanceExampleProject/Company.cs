1dd965efbe1e2e8379b4c78822bdf2c9c7d60536
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2021-10-16 10:04:35 +0300

    Inheritance

---
+++ InheritanceExampleProject/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace InheritanceTask
+{
+    public class Company
+    {
+        private readonly Employee[] employees;
+
+
+        public Company(Employee[] employees)
+        {
+            this.employees = employees;
+        }
+
+        public void GiveEverybodyBonus(decimal companyBonus)
+        {
+            foreach (Employee employee in employees)
+            {
+                employee.SetBonus(companyBonus);
+            }
+        }
+
+        public decimal TotalToPay() => employees.Sum(emp => emp.ToPay());
+
+
+        public string NameMaxSalary() => employees.OrderByDescending(emp => emp.ToPay()).First().Name;
+    }
+}
